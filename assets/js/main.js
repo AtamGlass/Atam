d14046a79ead57b7c8914bc4e89cb348b1a0ddeb
Atajan97 <146335117+Atajan97@users.noreply.github.com>
2023-11-07 16:12:47 +0500

    js_manu_active

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,16 +26,66 @@ function checkScroll(scrollPos){
 // Smooth scroll==============================================>
 $("[data-scroll]").on("click", function(event){
     event.preventDefault();
+    
 
-    let blockId = $(this).data('scroll');
-    let elementOfSet = $(blockId).offset().top;
+    let $this = $(this),
+        blockId = $this.data('scroll'),
+        elementOfSet = $(blockId).offset().top;
+
+    $(".header_nav .nav_link").removeClass("nav_active");
+    $this.addClass("nav_active");
 
     $("html, body").animate({
         scrollTop: elementOfSet + 1
     }, 1000);
+
+    setTimeout(function() {$this.removeClass("nav_active")}, 2000);
+
    
 });
 
+// let sections = document.querySelectorAll('section');
+// let navLink = document.querySelectorAll('.nav_link a');
+
+// console.log(navLink);
+
+
+//     window.addEventListener("scroll", () => {
+//         sections.forEach(sec => {
+//             let top = window.scrollY;
+//             let offset = sec.offsetTop;
+//             let height = sec.offsetHeight;
+//             let id = sec.getAttribute('id');
+
+//             if(top >= offset && top < offset + height){
+//                 navLink.forEach(link => {
+//                     link.classList.remove('nav_active');
+//                 });
+
+//                 document.querySelector(`.nav_link a[href='#${id}']`).classList.add('nav_active');
+//             };
+//         });
+
+//     });
+
+    // window.onscroll = () => {
+
+
+        // sections.forEach(sec => {
+        //     let top = window.scrollY;
+        //     let offset = sec.offsetTop - 150;
+        //     let height = sec.offsetHeight;
+        //     let id = sec.getAttribute('id');
+
+        //     if(top >= offset && top < offset + height){
+        //         navLink.forEach(link => {
+        //             link.classList.remove('nav_active');
+        //         });
+
+        //         document.querySelector(`.nav_link a[href='#${id}']`).classList.add('nav_active');
+        //     };
+        // });
+    // };
 
 
 
@@ -106,10 +156,6 @@ let videoPause = document.querySelector(".video_play_pause");
 let control = document.getElementsByTagName('controls');
 let productVideo = document.querySelector(".product_video");
 
-productVideo.addEventListener("mouseover", () => {
-    control.getAttribute.add('controls')
-})
-
 productVideo.addEventListener("mouseleave", () => {
     control.getAttribute.remove('controls')
 })
@@ -136,15 +182,14 @@ let  btn = document.querySelectorAll(".btn");
 let  portContentOne  = document.querySelectorAll(".port_content_one");
 let port_btn_door = document.querySelector(".port_btn_door");
 let port_btn_furniture = document.querySelector(".port_btn_furniture");
+let door = document.querySelector(".door");
+let furniture = document.querySelector(".furniture");
 
+
+console.log(btn);
 
 for(let i=0; i<btn.length; i++){
     btn[i].addEventListener('click', function(){
-
-        // for(let j=0; j<btn.length; j++){
-        //     btn[j].classList.remove('color');
-        // }
-        // this.classList.add('color');
         
         let caseFilter = this.getAttribute('case-filter');
 
@@ -153,14 +198,28 @@ for(let i=0; i<btn.length; i++){
             portContentOne[k].classList.add('change');
             port_btn_door.classList.add('color');
             port_btn_furniture.classList.remove('color');
+            door.classList.add('nav_active');
+            furniture.classList.remove('nav_active');
+            setTimeout(function() {
+                door.classList.remove("nav_active")
+            }, 2000);
 
             if( portContentOne[k].getAttribute('case-item') == caseFilter){
                 portContentOne[k].classList.remove('change');
                 portContentOne[k].classList.add('active');
                 port_btn_door.classList.remove('color');
                 port_btn_furniture.classList.add('color');
-            }
-        }
-    })
-}
+                door.classList.remove('nav_active');
+                furniture.classList.add('nav_active');
+                setTimeout(function() {
+                    furniture.classList.remove("nav_active")
+                }, 2000);    
+            };  
+        }; 
+    });
+    
+    
+   
+};
+
 
