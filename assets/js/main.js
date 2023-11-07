9047153d986835468bfd30fc067122bfe1345282
Atajan97 <146335117+Atajan97@users.noreply.github.com>
2023-11-07 12:25:00 +0500

    photo change

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,180 +134,33 @@ videoPlay.addEventListener("click", () => {
 // photo change ==============================================>
 let  btn = document.querySelectorAll(".btn");
 let  portContentOne  = document.querySelectorAll(".port_content_one");
-let portfolioBtn = document.querySelector("#portfolio_btn");
-let furnitureBtn = document.querySelector("#furniture_btn");
-let portBtnDoor = document.querySelector(".port_btn_door");
-let portBtnFur = document.querySelector(".port_btn_furniture");
-let port_one = document.querySelectorAll("#port_one");
-let port_two = document.querySelectorAll("#port_two");
-
-// console.log(portfolioBtn);
-// console.log(furnitureBtn);
-// console.log(portBtnDoor);
-// console.log(portBtnFur);
-console.log(port_one);
-console.log(port_two);
-
-
-portfolioBtn.addEventListener("click", () => {
-
-    console.log("yok");
-   
-    for(let i = 0; i<port_two.length; i++){
-        if(portBtnFur.classList != ('active')){
-            portBtnDoor.classList.add('color');
-            portBtnFur.classList.remove('color');
-            port_two[i].classList.add('active'); 
-            port_one[i].classList.remove('active'); 
-            
-        }else{
-            portBtnDoor.classList.remove('color');
-            portBtnFur.classList.add('color');
-            // port_two[i].classList.add('active');  
-            // port_one[i].classList.remove('active');
-        }
-        
-    }
-    
-})
-
-furnitureBtn.addEventListener("click", () => {
-
-    console.log("howa");
-    
-    for(let i = 0; i<port_one.length; i++){
-        if(portBtnDoor.classList != ('active')){
-            portBtnDoor.classList.remove('color');
-            portBtnFur.classList.add('color');
-            port_one[i].classList.add('active'); 
-            port_two[i].classList.remove('active'); 
-            
-    
-        }else{
-            portBtnDoor.classList.add('color');
-            portBtnFur.classList.remove('color');
-            // port_one[i].classList.remove('active');  
-            // port_two[i].classList.add('active');  
-            
-        }
-    }
-
-})
-
-
-
-
-
-
-
-
-
+let port_btn_door = document.querySelector(".port_btn_door");
+let port_btn_furniture = document.querySelector(".port_btn_furniture");
 
 
 for(let i=0; i<btn.length; i++){
     btn[i].addEventListener('click', function(){
 
-        for(let j=0; j<btn.length; j++){
-            btn[j].classList.remove('color');
-        }
-        this.classList.add('color');
+        // for(let j=0; j<btn.length; j++){
+        //     btn[j].classList.remove('color');
+        // }
+        // this.classList.add('color');
         
         let caseFilter = this.getAttribute('case-filter');
 
         for(let k=0; k<portContentOne.length; k++){
             portContentOne[k].classList.remove('active');
             portContentOne[k].classList.add('change');
+            port_btn_door.classList.add('color');
+            port_btn_furniture.classList.remove('color');
 
             if( portContentOne[k].getAttribute('case-item') == caseFilter){
                 portContentOne[k].classList.remove('change');
                 portContentOne[k].classList.add('active');
+                port_btn_door.classList.remove('color');
+                port_btn_furniture.classList.add('color');
             }
         }
     })
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// zoom effect ===============================================>
-
-// function imageZoom (imgId, resultId){
-//     let img, lens, result, cx, cy;
-//     img = document.getElementById(imgId);
-//     result = document.getElementById(resultId);
-
-//     lens = document.createElement("DIV"); 
-//     lens.setAttribute("class", "img_zoom_lens");
-
-//     img.parentElement.insertBefore(lens, img);
-//     cx = result.offsetWidth / lens.offsetWidth;
-//     cy = result.offsetHeight / lens.offsetHeight;
-
-//     result.style.backgroundImage = "url('" + img.src + "')";
-//     result.style.backgroundSize = (img.width * cx) + "px" + (img.height * cy) + "px";
-
-//     lens.addEventListener("mousemove", moveLens);
-//     img.addEventListener("mousemove", moveLens);
-
-//     lens.addEventListener("touchmove", moveLens);
-//     img.addEventListener("touchmove", moveLens);
-
-//     function moveLens(e){
-//         let pos, x, y;
-//         pos = getCursorPos(e);
-//         x = pos.x - (lens.offsetWidth / 2);
-//         y = pos.y - (lens.offsetHeight / 2);
-
-//         if (x > img.width - lens.offsetWidth) { x = img.width - img.offsetWidth;}
-//         if (x < 0) { x = 0;}
-//         if (y > img.height - lens.offsetHeight) { y = img.height - img.offsetHeight;}
-//         if (y < 0) { y = 0;}
-
-//         lens.style.left = x + "px";
-//         lens.style.top = y + "px";
-//         result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
-//     }
-
-//     function getCursorPos(e){
-//         var a, x = 0, y = 0 ;
-//         e = e || window.Event;
-//         a = img.BoundingClientRect();
-//         x = e.pageX - a.left;
-//         y = e.pageY - a.top;
-//         x = x - window.pageX;
-//         y = y - window.pageY;
-//         return { x : x, y : y };
-//     }
-// }
